1a4bf6cc3748381f0e63bd079fd545b730f19b1c
Penjat <theSpencerSymington@gmail.com>
2025-07-12 11:45:35 -0700

    Swift package structure.

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CombineBluetoothService",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "CombineBluetoothService",
+            targets: ["CombineBluetoothService"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "CombineBluetoothService"),
+        .testTarget(
+            name: "CombineBluetoothServiceTests",
+            dependencies: ["CombineBluetoothService"]),
+    ]
+)
