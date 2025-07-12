c3c00890019bbe70290bb947fa8283af329b88fb
Penjat <theSpencerSymington@gmail.com>
2025-07-12 13:25:08 -0700

    Added Central Service.

---
+++ Sources/CombineBluetoothService/CentralService.swift
@@ -0,0 +1,210 @@
+import CoreBluetooth
+import Combine
+
+protocol CentralService {
+    var events: PassthroughSubject<CentralServiceEvent, Never> { get }
+    var centralState: CurrentValueSubject<CBManagerState, Never> { get }
+    
+    func process(intent: CentralServiceIntent)
+}
+
+enum CentralServiceEvent {
+    case didDiscover(peripheral: CBPeripheral)
+    case didConnect(peripheral: CBPeripheral)
+    case didDisconnect(peripheral: CBPeripheral)
+    case recievedData(peripheralId: UUID, characteristicId: CBUUID, data: Data)
+    case scanning(Bool)
+}
+
+enum CentralServiceIntent {
+    case startScanning
+    case stopScanning
+    case send(peripheralId: UUID, serviceUUID: CBUUID, characteristicId: CBUUID, data: Data)
+}
+
+class BluetoothCentralService: NSObject, CentralService {
+    
+    public let input = PassthroughSubject<CentralServiceIntent, Never>()
+    public let events = PassthroughSubject<CentralServiceEvent, Never>()
+    public let centralState = CurrentValueSubject<CBManagerState, Never>(.unknown)
+    
+    private var centralManager: CBCentralManager!
+    
+    private var bag = Set<AnyCancellable>()
+    
+    private var serviceIDs: [CBUUID]!
+    private var characteristicIDs: [CBUUID]!
+    
+    private var peripherals = [CBPeripheral]()
+    
+    init(serviceIDs: [CBUUID], characteristicIDs: [CBUUID]) {
+        super.init()
+        print("creating...")
+        self.serviceIDs = serviceIDs
+        self.characteristicIDs = characteristicIDs
+        
+        centralManager = CBCentralManager(delegate: self, queue: nil, options: [CBCentralManagerOptionShowPowerAlertKey: true])
+        
+        input.sink { [weak self] in self?.process(intent: $0)}.store(in: &bag)
+        
+    }
+    
+    internal func process(intent: CentralServiceIntent) {
+        switch intent {
+        case .startScanning:
+            centralManager.scanForPeripherals(withServices: serviceIDs,
+                                                   options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
+            events.send(.scanning(true))
+            
+        case .stopScanning:
+            centralManager.stopScan()
+            events.send(.scanning(false))
+            
+        case .send(peripheralId: let peripheralId, serviceUUID: let serviceUUID, characteristicId: let characteristicId, data: let data):
+            print("sending data: \(data)")
+            send(peripheralUUID: peripheralId, serviceUUID: serviceUUID, characteristicId: characteristicId, data: data)
+        }
+    }
+    
+    private func send(peripheralUUID: UUID, serviceUUID: CBUUID, characteristicId: CBUUID, data: Data) {
+        guard
+            let peripheral = peripherals.first(where: { $0.identifier == peripheralUUID }),
+            let service = peripheral.services?.first(where: { $0.uuid == serviceUUID }),
+            let characteristic = service.characteristics?.first(where: { $0.uuid == characteristicId })
+        else {
+            return
+        }
+        peripheral.writeValue(data, for: characteristic, type: .withoutResponse)
+        print("Data was sent")
+    }
+    
+    private func connectToPeripheral(uuid: UUID, connect: Bool) {
+        for peripheral in peripherals where uuid == peripheral.identifier {
+            if connect {
+                centralManager.connect(peripheral, options: nil)
+            } else {
+                centralManager.cancelPeripheralConnection(peripheral)
+            }
+        }
+    }
+    
+    private func connectToCharacteristic(uuid: CBUUID, connect: Bool) {
+        
+        peripherals.forEach { peripheral in
+            peripheral.services?.forEach{ service in
+                guard let serviceCharacteristics = service.characteristics else {
+                    return
+                }
+                for characteristic in serviceCharacteristics where uuid == characteristic.uuid {
+                    peripheral.setNotifyValue(connect, for: characteristic)
+                    print("discovered characteristic: \(characteristic) \(characteristic.uuid)")
+                }
+            }
+        }
+    }
+}
+
+extension BluetoothCentralService: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        centralState.send(centralManager.state)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,
+                        advertisementData: [String: Any], rssi RSSI: NSNumber) {
+        if !peripherals.contains(peripheral) {
+            centralManager.connect(peripheral, options: nil)
+            peripherals.append(peripheral)
+            events.send(.didDiscover(peripheral: peripheral))
+            
+        } else if peripheral.state == .disconnected {
+            centralManager.connect(peripheral, options: nil)
+            events.send(.didDiscover(peripheral: peripheral))
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        peripheral.delegate = self
+        peripheral.discoverServices(serviceIDs)
+        events.send(.didConnect(peripheral: peripheral))
+    }
+}
+
+extension BluetoothCentralService: CBPeripheralDelegate {
+    func peripheral(_ peripheral: CBPeripheral, didModifyServices invalidatedServices: [CBService]) {
+        for service in invalidatedServices where serviceIDs.contains(service.uuid) {
+            peripheral.discoverServices(serviceIDs)
+        }
+        
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let error = error {
+            print("\(error)")
+            return
+        }
+        
+        guard let peripheralServices = peripheral.services else { return }
+        for service in peripheralServices {
+            peripheral.discoverCharacteristics(nil, for: service)
+            print("discovered service: \(service)")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: (any Error)?) {
+        events.send(.didDisconnect(peripheral: peripheral))
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if let error = error {
+            print("\(error)")
+            return
+        }
+        
+        guard let serviceCharacteristics = service.characteristics else {
+            return
+        }
+        
+        for characteristic in serviceCharacteristics where characteristicIDs.contains(characteristic.uuid) {
+            peripheral.setNotifyValue(true, for: characteristic)
+            events.send(.didConnect(peripheral: peripheral))
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let error = error {
+            print("Error receiving data: \(error.localizedDescription)")
+            return
+
+        }
+
+        guard let characteristicData = characteristic.value else {
+            print("No data received")
+            return
+
+        }
+        
+        events.send(.recievedData(peripheralId: peripheral.identifier, characteristicId: characteristic.uuid, data: characteristicData))
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: Error?) {
+        
+        if let error {
+            print("\(error)")
+            return
+        }
+        
+        if characteristic.isNotifying {
+            // Notification has started
+            
+        } else {
+            // Notification has stopped, so disconnect from the peripheral
+            
+//            cleanup()
+        }
+        
+    }
+    
+    func peripheralIsReady(toSendWriteWithoutResponse peripheral: CBPeripheral) {
+        
+    }
+}
